cbdf44f8d4831c17b4bdcb93bba1ec4f7e0565d5
Nikita Shcherbakov <shcerbackow@yandex.ru>
2021-04-12 13:59:42 +0300

    Added Spring files generated with Spring Initializr

---
+++ src/main/java/com/nikshcherbakov/vacanciesfinder/VacanciesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.nikshcherbakov.vacanciesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanciesFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacanciesFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikshcherbakov/vacanciesfinder/VacanciesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikshcherbakov.vacanciesfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
