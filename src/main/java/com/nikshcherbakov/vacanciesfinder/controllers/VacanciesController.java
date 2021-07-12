589c0f6ff77cc9eca4d776b7eecce983c13bd549
Nikita Shcherbakov <shcerbackow@yandex.ru>
2021-07-13 02:31:45 +0300

    Changed header.html, added submenu

--- src/main/java/com/nikshcherbakov/vacanciesfinder/controllers/VacanciesController.java
+++ src/main/java/com/nikshcherbakov/vacanciesfinder/controllers/VacanciesController.java
@@ -1,10 +1,20 @@
 package com.nikshcherbakov.vacanciesfinder.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VacanciesController {
 
+    @GetMapping("/vacancies")
+    public String showVacanciesPage(Model model) {
+        // TODO MAIN_PRIORITY 1. Продумать регулярный подбор вакансий с помощью сервиса HeadHunterService
+        // TODO MAIN_PRIORITY 2. Продумать отображение вакансий в виде таблицы
+        // TODO MAIN_PRIORITY 3. Реазизовать getmapping на /vacancies и отображение найденных вакансий
+        return "vacancies";
+    }
 
+    // TODO MAIN_PRIORITY 4. Реализовать getmapping на /vacancies/favorites
 
 }
